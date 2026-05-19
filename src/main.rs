903051bc62f5c84948ec6b9fff9775d549ad3286
Emanuel Farias <eafarias.ci@gmail.com>
2026-05-19 09:57:53 -0300

    Agregue Enunciado de Ejercicio 1 y Solución

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// Ejercicio 1)
+// Escribir un algoritmo que permite asignar los numeros 10, 15 y 20 a tres
+// variables numericas. Calcule y muestre el promedio de ellas.
+
 fn main() {
-    println!("Hello, world!");
+    let diez: i32 = 10;
+    let quince: i32 = 15;
+    let veinte: i32 = 20;
+    let promedio: i32 = (diez + quince + veinte) / 3;
+    println!("El Valor Promedio de las variables diez, quince y veinte es: {promedio}");
 }
